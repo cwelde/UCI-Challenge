03068e27591216c9f69fb4bd75011d27b1efd44c
Chris <cwelde@uci.edu>
2018-02-11 21:12:21 -0800

    Fixed program to automatically run two given csv files.

--- tst/tst_comp_csv.py
+++ tst/tst_comp_csv.py
@@ -13,7 +13,10 @@ def compareFiles(file1, file2):
        return 1
 
 
-getFile1 = input('Enter the name of the first file: ')
-getFile2 = input('Enter the name of the second file: ')
+#getFile1 = input('Enter the name of the first file: ')
+#getFile2 = input('Enter the name of the second file: ')
+#commenting this out to be able to run a tests
+getFile1 = 'angles_UCI_CS.csv'
+getFile2 = 'angles_UCI_CS_out.csv'
 
 print(compareFiles(getFile1, getFile2))
