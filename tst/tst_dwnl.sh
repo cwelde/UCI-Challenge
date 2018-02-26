e0cae0ffd0ea01ab5a863f212c3e3bf745584fbb
Chris <cwelde@uci.edu>
2018-02-25 21:00:11 -0800

    Fixed typo

--- tst/tst_dwnl.sh
+++ tst/tst_dwnl.sh
@@ -1,2 +1,2 @@
-wget http://rapid-hub.org/data/angles_UCI_CS_out.csv
-wget http://rapid-hub.org/data/angles_UCI_CS.csv
+- wget http://rapid-hub.org/data/angles_UCI_CS_out.csv
+- wget http://rapid-hub.org/data/angles_UCI_CS.csv
